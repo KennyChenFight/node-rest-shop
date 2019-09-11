da98954f6202122882434454a091b8d90f0bcdf1
KennyChen <kennychen851228@gmail.com>
2019-09-11 20:46:11 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.post('/postTesting', (req, res, next) => {
+    res.json(req.body);
+});
 
 app.get('/', function (req, res, next) {
     res.send('Hello World');
