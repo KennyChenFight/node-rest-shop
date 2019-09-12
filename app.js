549d070ecdbd2c431537a1d0d22e19daff757ce9
kenny <kennychen851228@gmail.com>
2019-09-12 14:22:03 +0800

    連接mongodb

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dbUrl = process.env.MONGODB_URL;
+
+// mongoose.connect(dbUrl, {keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+// const connection = mongoose.connection;
+// connection.on('error', console.error.bind(console, 'connection error:'));
+// connection.once('open', function () {
+//     console.log('mongodb is connected');
+// });
 
 const express = require('express');
 const app = express();
@@ -15,6 +30,18 @@ app.get('/', function (req, res, next) {
     res.send('Hello World');
 });
 
-app.listen(port, function () {
-    console.log('Express app started on ' + port);
-});
+connectDB();
+
+function connectDB() {
+    mongoose.connection
+        .on('error', console.error.bind(console, 'connection error:'))
+        .on('disconnected', connectDB)
+        .once('open', listen);
+    return mongoose.connect(dbUrl, {keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true});
+}
+
+function listen() {
+    app.listen(port, function () {
+        console.log('Express app started on ' + port);
+    });
+}
